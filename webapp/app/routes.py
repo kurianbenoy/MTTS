2342f61e874692525ed983c1acfec601ccbea327
kurianbenoy <kurian.bkk@gmail.com>
2020-04-24 23:08:33 +0530

    calling espeak and saving in flask

--- webapp/app/routes.py
+++ webapp/app/routes.py
@@ -1,17 +1,19 @@
 from flask import render_template, redirect, request
 from app.forms import TextForm
 from app import app
+import os
 
 @app.route('/text')
 def input():
-    return render_template('index.html')
+    return render_template('output.html')
 
 @app.route('/input', methods=['GET','POST'])
 def text_input():
     form = TextForm()
     if form.validate_on_submit():
         print("form correct")
-        # text = request.POST['text']
-        # print(text)
+        text = request.form['text']
+        print(text)
+        os.system(f"espeak {text} -w a.wav")
         return redirect("/text")
     return render_template('input.html', form=form)
