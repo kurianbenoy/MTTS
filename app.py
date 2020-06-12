28d1389aca405c5ad65294b26142e9f1f2d8e6c8
kurianbenoy <kurian.bkk@gmail.com>
2020-06-12 22:11:02 +0530

    update comment

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ def predict():
     # 	my_prediction = classifier.predict(vect)
     return render_template('output.html')
     #os.system(f"./flite/bin/flite flite/doc/alice")
+    #./flite/bin/flite   "-voice" flite/voices/cmu_indic_kan_plv.flitevox "അണ<0d4d>ണാൻ ക<0d41>ഞ<0d4d>ഞ<0d41>ം തന<0d4d>നാലായത<0d4d>."
+    # ./flite/bin/flite --setf duration_stretch=1.5  "-voice" flite/voices/cmu_indic_kan_plv.flitevox "അണ<0d4d>ണാൻ ക<0d41>ഞ<0d4d>ഞ<0d41>ം തന<0d4d>നാലായത<0d4d>
 
 if __name__ == "__main__":
     app.run(debug=True)
