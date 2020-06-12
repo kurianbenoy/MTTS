9d29d57838ede4302d1b43f5dac82b192561ec0f
kurianbenoy <kurian.bkk@gmail.com>
2020-06-12 10:32:44 +0530

    add single webapp file

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def predict():
     	vect = cv.transform(data).toarray()
     	my_prediction = classifier.predict(vect)
     	return render_template('result.html', prediction=my_prediction)
-
+        os.system(f"./flite/bin/flite flite/doc/alice")
 
 if __name__ == "__main__":
     app.run(debug=True)
