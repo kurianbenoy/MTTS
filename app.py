589da09d1917745b8f98b336d166914e0da8a7c2
kurianbenoy <kurian.bkk@gmail.com>
2020-06-12 10:47:48 +0530

    update flask app

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('input.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    if request.method == 'POST':
-    	message = request.form['message']
-    	data = [message]
-    	vect = cv.transform(data).toarray()
-    	my_prediction = classifier.predict(vect)
-    	return render_template('result.html', prediction=my_prediction)
-        os.system(f"./flite/bin/flite flite/doc/alice")
+    # if request.method == 'POST':
+    # 	message = request.form['message']
+    # 	data = [message]
+    # 	vect = cv.transform(data).toarray()
+    # 	my_prediction = classifier.predict(vect)
+    return render_template('output.html')
+    #os.system(f"./flite/bin/flite flite/doc/alice")
 
 if __name__ == "__main__":
     app.run(debug=True)
