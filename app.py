eab2ee25fdea2f34a6c8cd03f4e1a22780c450e8
kurianbenoy <kurian.bkk@gmail.com>
2020-06-12 10:30:21 +0530

    update file

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST':
+    	message = request.form['message']
+    	data = [message]
+    	vect = cv.transform(data).toarray()
+    	my_prediction = classifier.predict(vect)
+    	return render_template('result.html', prediction=my_prediction)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
