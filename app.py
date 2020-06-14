cbaa15326c207bd2ae2cb01d2d3c9016fe976d55
kurianbenoy <kurian.bkk@gmail.com>
2020-06-14 11:02:57 +0530

    add tamil output voice

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ app = Flask(__name__)
 def home():
     return render_template('input.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/',methods=['POST'])
 def predict():
     if request.method == 'POST':
         message = request.form['message']
         print(message)
         print(type(message))
-        Popen(["./flite/bin/flite", "-voice", "flite/voices/cmu_indic_kan_plv.flitevox", \
+        Popen(["./flite/bin/flite", "-voice", "flite/voices/cmu_indic_tam_sdr.flitevox", \
              message,])
     # 	vect = cv.transform(data).toarray()
     # 	my_prediction = classifier.predict(vect)
-    return render_template('output.html')
+    return render_template('output.html', message)
 
 
 if __name__ == "__main__":
