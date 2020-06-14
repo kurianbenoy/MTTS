d8dd8e248ceb727558df2382a27bddf71a04d6e2
kurianbenoy <kurian.bkk@gmail.com>
2020-06-14 11:42:17 +0530

    made audio worlking

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ def predict():
         print(type(message))
         Popen(["./flite/bin/flite", "-voice", "flite/voices/cmu_indic_tam_sdr.flitevox", \
              message,])
+        Popen(["./flite/bin/flite", "-voice", "flite/voices/cmu_indic_tam_sdr.flitevox", \
+             message, "-w", "static/op.wav"])
     # 	vect = cv.transform(data).toarray()
     # 	my_prediction = classifier.predict(vect)
     return render_template('output.html', message=message)
